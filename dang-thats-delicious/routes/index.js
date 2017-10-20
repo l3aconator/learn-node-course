593dd32cc616cf1f891e6a935bc22096cf8d1fac
Kyle Bacon <hello@kylebaconcreative.com>
2017-10-20 14:17:51 -0700

    ep4

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
+// every time console.log in express, it logs in node machine rather than frontend
+
+// --- can only send on response, or else it will error
+
+// --- req has all the request info, request
+// --- res has all the methods to send data back, response
+
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+    console.log('hey!!!');
+    const wes = { name: 'wes', age: 100, cool: true};
+
+    // res.send('Hey! it works!');
+    // res.json(wes);
+
+    // --- takes data from query string http://localhost:7777/?name=wes&age=100&cool=true
+    // res.send(req.query.name);
+    // res.json(req.query);
+});
+
+router.get('/reverse/:name', (req, res) => {
+    const reverse = [...req.params.name].reverse().join('');
+    res.send(reverse);
 });
 
 module.exports = router;
