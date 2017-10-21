14c3b8d1f34309676179b0a588f90c467d299da6
Kyle Bacon <hello@kylebaconcreative.com>
2017-10-21 11:33:29 -0700

    ep14

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -20,3 +20,23 @@ exports.getStores = async (req, res) => {
     const stores = await Store.find();
     res.render('stores', { title: 'Stores', stores })
 }
+
+exports.editStore = async (req, res) => {
+    // 1. Find the store given the id
+    const store = await Store.findOne({ _id: req.params.id });
+    // 2. configure they are the owner of the store
+    // TODO
+    // 3. Render out the edit form so the user can update their store
+    res.render('editStore', { title: `Edit ${store.name}`, store });
+}
+
+exports.updateStore = async (req, res) => {
+    // 1. find and update the store
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true, // return the new store instead of the old owner
+        runValidators: true
+    }).exec();
+    // 2. redirect them the store and tell them it worked.
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store »</a>`);
+    res.redirect(`/stores/${store._id}/edit`);
+}
