e780f3014241c45f32e73218882b5e58da430ae0
Kyle Bacon <hello@kylebaconcreative.com>
2017-10-23 16:21:20 -0700

    ep20

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -37,15 +37,21 @@ const storeSchema = new mongoose.Schema({
     photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
     if(!this.isModified('name')) {
         next(); // skip it
         return; // stop this function from running
     }
     this.slug = slug(this.name);
-    next();
 
-    // TODO: make more resiliant so slugs are unique
+    // find other stores that have a slug of wes, wes-1, wes-2
+    const slugRegEx = new RegExp(`^(${this.slug}((-[0-9]*$)?)$)`, 'i');
+    const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+
+    if(storesWithSlug.length) {
+        this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+    }
+    next();
 });
 
 module.exports = mongoose.model('Store', storeSchema);
