7e227292f8bf5d6321d101c2c74b96614fdc99c5
l3acon <hello@kylebaconcreative.com>
2017-10-24 08:18:29 -0700

    save user auth to database

--- dang-thats-delicious/controllers/userController.js
+++ dang-thats-delicious/controllers/userController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const Store = mongoose.model('Store');
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 exports.loginForm = (req, res) => {
     res.render('login', {title: 'Login'})
@@ -31,4 +32,11 @@ exports.validateRegister = (req, res, next) => {
     }
 
     next(); // there were no errors!
-}
+};
+
+exports.register = async (req, res, next) => {
+    const user = new User({ email: req.body.email, name: req.body.name });
+    const register = promisify(User.register, User);
+    await register(user, req.body.password);
+    next(); // pass to authController.login
+};
