0321efd2ef65aa6a6f16ca9592e7c59c06b296d1
Kyle Bacon <hello@kylebaconcreative.com>
2017-10-24 20:06:39 -0700

    ep25

--- dang-thats-delicious/controllers/authController.js
+++ dang-thats-delicious/controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
     successRedirect: '/',
     successFlash: 'You are logged in!'
 });
+
+exports.logout = (req, res) => {
+    req.logout();
+    req.flash('success', 'You are now logged out!');
+    res.redirect('/')
+};
+
+exports.isLoggedIn = (req, res, next) => {
+    // first check if the user is authed
+    if(req.isAuthenticated()){
+        next(); //carry on! they are logged in
+        return;
+    }
+    req.flash('error', 'Oops you must be logged in to do that!');
+    res.redirect('/login');
+};
